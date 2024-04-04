cbdede122fdae49763dfc19096f03f64323368f7
Daniil Klyutko <klyutko.d@gmail.com>
2024-04-04 19:16:57 +0600

    fwafawfawfaw

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-import os
+import re
+
+from httpcore import TimeoutException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from telebot import TeleBot
+import os
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 driver = webdriver.Chrome(options=chrome_options)
 print("Браузер успешно открыт")
@@ -17,17 +22,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home_view():
-    return "<h1>Welcome to Geeks for Geeks</h1>"
-
 
 @app.route('/post_endpoint', methods=['POST'])
 def handle_post():
-    url = request.json.get('url')
+    url = request.json
     print(url)
 
     driver.get(url)
+
     try:
         price_element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.TAG_NAME, "ins"))
@@ -35,11 +37,14 @@ def handle_post():
 
         price = price_element.text
 
+
     except Exception as e:
-        print("Ошибка при поиске цены на странице:", e)
-        return jsonify({'status': 'error', 'message': 'Ошибка при поиске цены на странице'})
+        print("Ошибка при поиске заголовка страницы:", e)
+        return None
 
     response_data = {'status': 'success', 'message': f'{price}'}
     return jsonify(response_data)
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
