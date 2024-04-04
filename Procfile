5ea9eba25eb1d4cc05e6394c6e88bda53f311bae
Daniil Klyutko <klyutko.d@gmail.com>
2024-04-04 17:17:37 +0600

    fwafawfawfaw

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-bot: python main.py
+start: python main.py
--- main.py
+++ main.py
@@ -18,76 +18,31 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(options=chrome_options)
 print("Браузер успешно открыт")
 
-# Функция для открытия браузера и выполнения процедуры
-def open_browser_and_process(url):
-    # Открытие веб-страницы
-    driver.get(url)
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+
+@app.route('/post_endpoint', methods=['POST'])
+def handle_post():
+    url = request.json.get('url')
+    print(url)
+
+    driver.get(url)
     try:
         price_element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.TAG_NAME, "ins"))
         )
 
-        title_element = WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.TAG_NAME, "h1"))
-        )
-
-        # Извлечение текста элемента
         price = price_element.text
-        title = title_element.text
-
-        # Возвращаем извлеченный текст
-        return title, price
 
     except Exception as e:
-        print("Ошибка при поиске заголовка страницы:", e)
-        return None
+        print("Ошибка при поиске цены на странице:", e)
+        return jsonify({'status': 'error', 'message': 'Ошибка при поиске цены на странице'})
+
+    response_data = {'status': 'success', 'message': f'{price}'}
+    return jsonify(response_data)
 
 
-# Инициализация бота
-bot = TeleBot('7185448364:AAFKMQUW1Z6_oJcOYnK8MxTbSLES-yiDlK0')
-
-
-# Обработчик команды '/start'
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    bot.send_message(message.chat.id,
-                     "Привет! Отправь мне сообщение с текстом '/get_price [ссылка]', чтобы получить цену товара.")
-
-
-# Обработчик команды '/get_price'
-@bot.message_handler(commands=['get_price'])
-def handle_get_price(message):
-    # Получаем текст сообщения пользователя
-    text = message.text
-
-    # Проверяем, содержит ли сообщение ссылку
-    url_match = re.search(r'(https?://\S+)', text)
-    if url_match:
-        url = url_match.group(1)  # Извлекаем ссылку из сообщения
-        # Открываем браузер и получаем цену товара
-        title, price = open_browser_and_process(url)
-        if title and price:
-            bot.send_message(message.chat.id, f"Цена товара: {price}\nНазвание товара: {title}")
-        else:
-            bot.send_message(message.chat.id, "Не удалось получить информацию о товаре.")
-    else:
-        bot.send_message(message.chat.id,
-                         "Не найдена ссылка на товар. Пожалуйста, отправьте ссылку в формате: /get_price [ссылка]")
-
-
-# Обработчик ошибочных команд
-@bot.message_handler(func=lambda message: True)
-def handle_invalid(message):
-    bot.reply_to(message, "Неизвестная команда. Используйте '/start' для начала.")
-
-
-@bot.message_handler(commands=['check_browser'])
-def handle_check_browser(message):
-    driver.get("https://www.python.org")
-    title = driver.title
-    bot.send_message(message.chat.id, f"Заголовок страницы Python.org: {title}")
-
-
-# Запуск бота
-bot.polling()
+if __name__ == '__main__':
+    app.run(debug=True)
