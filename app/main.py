88b0c6e0778892e9acc1859f5eecd5fe228462da
Daniil Klyutko <95645570+klyutko7p@users.noreply.github.com>
2024-09-12 17:17:37 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ import os
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(options=chrome_options)
