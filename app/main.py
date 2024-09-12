c6f55e406a9854b38f298085c3d137e3fb5d8d34
Daniil Klyutko <95645570+klyutko7p@users.noreply.github.com>
2024-09-12 17:13:50 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,10 +9,7 @@ import os
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless=new")
-chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-gpu")
 
 driver = webdriver.Chrome(options=chrome_options)
 print("Браузер успешно открыт")
