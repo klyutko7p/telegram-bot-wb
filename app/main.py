6b55fc686da71c7c6d523963b177083371c87477
Daniil Klyutko <95645570+klyutko7p@users.noreply.github.com>
2024-09-12 17:27:22 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 import re
 
+from httpcore import TimeoutException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from telebot import TeleBot
 import os
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 driver = webdriver.Chrome(options=chrome_options)
 print("Браузер успешно открыт")
@@ -20,15 +22,14 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route('/post_endpoint', methods=['POST'])
 def handle_post():
     url = request.json['url']
     print(url)
     driver.implicitly_wait(10)
     driver.get(url)
-
-    try:
+    
+    try:    
         price_element = driver.find_elements(By.CLASS_NAME, "price-block__final-price")[1].text
         price = price_element
 
@@ -39,6 +40,3 @@ def handle_post():
 
     response_data = {'status': 'success', 'message': f'{price}'}
     return jsonify(response_data)
-
-
-driver.quit()
